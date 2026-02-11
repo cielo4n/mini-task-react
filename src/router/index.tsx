f41aba8e8b0c5d9e0f2abf3ae00340f81875b24d
smyoon <cielo4n@gmail.com>
2026-02-11 16:14:42 +0900

    rename layout, tsx del

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from '@/pages/Login.tsx'
-import Tasks from '@/pages/Tasks.tsx'
-import TaskDetail from '@/pages/TaskDetail.tsx'
-import Layout from '@/layouts/Layout.tsx'
-import AuthGuard from '@/router/AuthGuard.tsx'
+import Login from '@/pages/Login'
+import Tasks from '@/pages/Tasks'
+import TaskDetail from '@/pages/TaskDetail'
+import Layout from '@/layouts/Layout'
+import AuthGuard from '@/router/AuthGuard'
 import MenuLayout from '@/layouts/MenuLayout'
 
 export default function AppRoutes(){
